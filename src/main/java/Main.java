18041e82821b55306d9b2f1f90a1dda501550611
pryaly <yulia.pryalkova@emc.com>
2016-12-28 17:59:31 +0300

    save work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,20 +14,23 @@ import java.util.stream.Collectors;
  */
 public class Main {
     private static final String BASE_URL = "https://www.wildberries.ru";
+    private static final String ADD_URL = "/catalog/zhenshchinam/bele-i-kupalniki/plyazhnaya-moda?pagesize=40&sort=popular";
 
     public static void main(String[] args) {
         if (args == null || args.length != 2) {
             System.out.println("USE webparser.java [path to file with result] [number of products (<= 40)]");
             System.exit(0);
         }
-        if (Integer.parseInt(args[1]) > 40) {
+        int nProducts = Integer.parseInt(args[1]);
+        if (nProducts > 40) {
             System.out.println("Number of products should be <= 40");
             System.exit(0);
         }
+        System.out.println("Start getting of " + nProducts + " products from " + BASE_URL + ADD_URL);
         Document doc = null;
         try {
             doc = Jsoup
-                    .connect(BASE_URL + "/catalog/zhenshchinam/bele-i-kupalniki/plyazhnaya-moda?pagesize=40&sort=popular").get();
+                    .connect(BASE_URL + ADD_URL).get();
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
@@ -35,7 +38,7 @@ public class Main {
 
         List<Element> brandNames = doc.getElementsByAttributeValue("class", "cg_list_item");
         List<Product> products = brandNames.stream().map(Main::createProduct).collect(Collectors.toList());
-        products = products.subList(0, Math.min(products.size(), Integer.parseInt(args[1])));
+        products = products.subList(0, Math.min(products.size(), nProducts));
         PrintWriter pw = null;
         try {
             pw = new PrintWriter(new FileOutputStream(args[0]));
@@ -44,6 +47,7 @@ public class Main {
             e.printStackTrace();
             System.exit(-1);
         }
+        System.out.println("Start saving of products to " + args[0]);
         for (Product product : products)
             pw.println(product.toString());
         pw.close();
