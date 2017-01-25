ddf5513cc09465ac9b8cba0146c56496c0bd96b6
pryaly <yulia.pryalkova@emc.com>
2017-01-25 19:51:08 +0300

    save work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,57 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 /**
  * Created by pryaly on 12/28/2016.
  */
 public class Main {
     private static final String BASE_URL = "https://www.wildberries.ru";
-    private static final String ADD_URL = "/catalog/zhenshchinam/bele-i-kupalniki/plyazhnaya-moda?pagesize=40&sort=popular";
+    private static final String NO_PRODUCT = "Продан";
+
+    private static Map<String, Product> products = new HashMap<>();
 
     public static void main(String[] args) {
         if (args == null || args.length != 2) {
-            System.out.println("USE webparser.java [path to file with result] [number of products (<= 40)]");
+            System.out.println("USE webparser.java [path to file with result] [link to first page of products]");
             System.exit(0);
         }
-        int nProducts = Integer.parseInt(args[1]);
-        if (nProducts > 40) {
-            System.out.println("Number of products should be <= 40");
-            System.exit(0);
-        }
-        System.out.println("Start getting of " + nProducts + " products from " + BASE_URL + ADD_URL);
+        String link = args[1] + "?pagesize=100";
+        System.out.println("Start getting of products from " + link);
         Document doc = null;
         try {
             doc = Jsoup
-                    .connect(BASE_URL + ADD_URL).get();
+                    .connect(link).userAgent("Mozilla/5.0").get();
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
         }
 
-        List<Element> brandNames = doc.getElementsByAttributeValue("class", "cg_list_item");
-        List<Product> products = brandNames.stream().map(Main::createProduct).collect(Collectors.toList());
-        products = products.subList(0, Math.min(products.size(), nProducts));
+        int numberOfPages = (int) Math.floor(Integer.parseInt(((TextNode) doc.select(".total.many").get(0).childNode(1)
+                .childNode(0)).text()) / 100.0);
+        for (int pageNumber = 1; pageNumber <= numberOfPages; pageNumber++) {
+            Elements elements = doc.getElementsByClass("dtList");
+            for (Element element : elements) {
+                createProducts(element);
+                if (products.size() >= 25){
+                    break;
+                }
+            }
+            if (products.size() >= 25){
+                break;
+            }
+        }
         PrintWriter pw = null;
         try {
             pw = new PrintWriter(new FileOutputStream(args[0]));
@@ -48,31 +61,62 @@ public class Main {
             System.exit(-1);
         }
         System.out.println("Start saving of products to " + args[0]);
-        for (Product product : products)
+        for (Product product : products.values())
             pw.println(product.toString());
         pw.close();
 
+
+
     }
 
-    private static Product createProduct(Element e) {
-        Product product = new Product();
-        product.setHref(e.getElementsByAttribute("href").attr("href"));
-        product.setName(e.getElementsByAttributeValue("class", "cg_c cg_brand").text());
+    protected static void createProducts(Element element) {
+        String[] articles = element.getElementsByAttribute("data-colors").attr("data-colors").split(",");
+        for (String article : articles) {
+            System.out.println("Start getting info for product with article " + article);
+            if (products.containsKey(article)) {
+                System.out.println("Product with this article already extracted");
+                continue;
+            }
+            Product product = new Product();
+            product.setArticle(article);
+            String url = createUrl(article);
+            product.setHref(url);
+            product.setName(element.getElementsByClass("brand-name").text());
 
-        getCount(product.getHref(), product);
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(url).userAgent("Mozilla/5.0").get();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
 
-        return product;
-    }
+            product.setOrdersCount(doc.getElementsByClass("j-orders-count").text());
+            product.setCost(doc.getElementById("Price").text());
+            product.setOriginalCost(doc.getElementsByTag("del").text());
+            product.setDiscount(doc.getElementsByClass("discount").text());
 
-    private static void getCount(String url, Product p) {
-        Document doc = null;
-        try {
-            doc = Jsoup.connect(BASE_URL + url).userAgent("Mozilla/5.0").get();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(-1);
+            Elements sizes = doc.getElementsByClass("j-size");
+            Map<String, String> restCount = new HashMap<>();
+            for (Element size : sizes) {
+                restCount.put(size.getElementsByTag("span").get(0).text(),
+                        size.select(".goods-balance.j-low-quantity").text());
+            }
+            Elements soldSizes = doc.select(".j-size.disabled.j-sold-out");
+            for (Element size : soldSizes) {
+                restCount.put(size.getElementsByTag("span").get(0).text(),
+                        NO_PRODUCT);
+            }
+            product.setRestCount(restCount);
+            products.put(article, product);
         }
-        p.setCount(doc.getElementsByAttributeValue("class", "j-orders-count").text());
-        p.setArticle(doc.getElementsByAttributeValue("class", "article j-article").text());
+
     }
+
+    private static String createUrl(String color) {
+        final String CATALOG_URL = "https://www.wildberries.ru/catalog/";
+        final String DETAIL_URL = "/detail.aspx";
+        return CATALOG_URL + color + DETAIL_URL;
+    }
+
 }
--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,6 +1,9 @@
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by pryaly on 12/28/2016.
  */
@@ -9,15 +12,24 @@ public class Product {
 
     private String href;
     private String name;
-    private String count;
+    private String ordersCount;
     private String article;
+    private String cost;
+    private String originalCost;
+    private String discount;
+    Map<String, String> restCount = new HashMap<>();
 
     @Override
     public String toString() {
         return "Product{" +
-                "name='" + name + '\'' +
-                ", count='" + count + '\'' +
+                "href='" + href + '\'' +
+                ", name='" + name + '\'' +
+                ", ordersCount='" + ordersCount + '\'' +
                 ", article='" + article + '\'' +
+                ", cost='" + cost + '\'' +
+                ", originalCost='" + originalCost + '\'' +
+                ", discount='" + discount + '\'' +
+                ", restCount=" + restCount +
                 '}';
     }
 }
