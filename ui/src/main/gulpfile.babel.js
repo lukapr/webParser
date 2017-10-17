22facc19b42127e68f93b857e3c38032aefc0ac4
pryaly <yulia.pryalkova@emc.com>
2017-10-17 13:16:36 +0300

    Add separate module for UI

--- ui/src/main/gulpfile.babel.js
+++ ui/src/main/gulpfile.babel.js
@@ -4,15 +4,15 @@ var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 
 gulp.task('build', function () {
-    return browserify({entries: './js/app.jsx', extensions: ['.jsx'], debug: true})
+    return browserify({entries: './webapp/resources/dev/app.jsx', extensions: ['.jsx'], debug: true})
         .transform(babelify.configure({presets: ['es2015', 'react', "stage-1"]}))
         .bundle()
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest('main/resources/static/dist'));
+        .pipe(gulp.dest('./resources/static/dist'));
 });
 
 gulp.task('watch', ['build'], function () {
-    gulp.watch('js/**/*.jsx', ['build']);
+    gulp.watch('./webapp/resources/dev/**/*.jsx', ['build']);
 });
 
 gulp.task('default', ['watch']);
